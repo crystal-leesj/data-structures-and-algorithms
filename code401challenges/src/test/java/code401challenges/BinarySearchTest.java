05a139ca7661897814bf6e011be0b814245c9b5a
Crystal Soo Lee <soo.leesj@gmail.com>
2020-01-09 00:23:29 -0800

    done

---
+++ code401challenges/src/test/java/code401challenges/BinarySearchTest.java
@@ -0,0 +1,21 @@
+package code401challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BinarySearchTest {
+    @Test public void testBinarySearch() {
+        int[] testArr = new int[] {4,8,15,16,23,42};
+        int testEle = 15;
+        int actual = BinarySearch.binarySearch(testArr, testEle);
+        assertEquals(2, actual);
+    }
+
+    @Test public void testBinarySearch2() {
+        int[] testArr = new int[] {11,22,33,44,55,66,77};
+        int testEle = 90;
+        int actual = BinarySearch.binarySearch(testArr, testEle);
+        assertEquals(-1, actual);
+    }
+}
