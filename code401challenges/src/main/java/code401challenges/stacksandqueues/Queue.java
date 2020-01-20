f9dc8c15a82ee339779bcc9ef83c64f7349a052d
Crystal Soo Lee <soo.leesj@gmail.com>
2020-01-20 01:23:18 -0800

    queue is done

--- code401challenges/src/main/java/code401challenges/stacksandqueues/Queue.java
+++ code401challenges/src/main/java/code401challenges/stacksandqueues/Queue.java
@@ -1,4 +1,51 @@
 package code401challenges.stacksandqueues;
 
-public class Queue {
+public class Queue<T> {
+
+    private Node<T> front;
+
+    public Queue() {
+        this.front = null;
+    }
+
+    public boolean isEmpty(){
+        return this.front == null;
+    }
+
+    public Node<T> peek() {
+        return front;
+    }
+
+    public int getSize() {
+        int size = 0;
+        Node<T> current = this.front;
+        while (current != null) {
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
+
+    // Define a method called enqueue which takes any value as an argument and adds a new node with that value to the back of the queue with an O(1) Time performance.
+    public void enqueue(T new_value) {
+        Node<T> new_node = new Node(new_value);
+        new_node.next = null;
+
+        if (this.front == null) {
+            this.front = new_node;
+        } else {
+            Node<T> current = this.front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new_node;
+        }
+    }
+
+    // Define a method called dequeue that does not take any argument, removes the node from the front of the queue, and returns the node’s value.
+    public T dequeue() {
+        Node<T> removedQueue = this.front;
+        this.front = front.next;
+        return removedQueue.value;
+    }
 }
--- code401challenges/src/test/java/code401challenges/stacksandqueues/QueueTest.java
+++ code401challenges/src/test/java/code401challenges/stacksandqueues/QueueTest.java
@@ -1,4 +1,51 @@
 package code401challenges.stacksandqueues;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class QueueTest {
+    @Test
+    public void testEnqueue() {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        assertEquals(1, queue.peek().value);
+        queue.enqueue(2);
+        assertNotEquals(2, queue.peek().value);
+        queue.enqueue(3);
+        assertNotEquals(3, queue.peek().value);
+        assertNotNull(queue);
+        assertEquals(3, queue.getSize());
+    }
+
+    @Test
+    public void testDequeue() {
+        Queue queue = new Queue();
+        assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        assertFalse(queue.isEmpty());
+        assertEquals(1, queue.getSize());
+        queue.enqueue(2);
+        assertEquals(2, queue.getSize());
+        queue.enqueue(3);
+        assertEquals(3, queue.getSize());
+
+        int actual = (int) queue.dequeue();
+        assertEquals(1, actual);
+        assertEquals(2, queue.getSize());
+        actual = (int) queue.dequeue();
+        assertEquals(2, actual);
+        assertEquals(1, queue.getSize());
+        queue.dequeue();
+        assertEquals(0, queue.getSize());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        Queue queue = new Queue();
+        assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        queue.enqueue(2);
+        assertFalse(queue.isEmpty());
+    }
 }
