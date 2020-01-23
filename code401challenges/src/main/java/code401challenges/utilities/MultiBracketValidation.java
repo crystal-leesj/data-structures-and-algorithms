0e36dd3c84c1bc611a4c9200c8e34f3b72c3cbb1
Crystal Soo Lee <soo.leesj@gmail.com>
2020-01-23 00:30:30 -0800

    completed

---
+++ code401challenges/src/main/java/code401challenges/utilities/MultiBracketValidation.java
@@ -0,0 +1,27 @@
+package code401challenges.utilities;
+
+import java.util.*;
+
+public class MultiBracketValidation {
+    public static boolean multiBracketValidation(String input) {
+
+        HashMap<Character,Character> hash_map = new HashMap<Character, Character>();
+        hash_map.put('(',')');
+        hash_map.put('[',']');
+        hash_map.put('{','}');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            if (hash_map.containsKey(ch)) {
+                stack.push(ch);
+            }
+            if (hash_map.containsValue(ch)) {
+                if (hash_map.get(stack.peek()) == ch)
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
