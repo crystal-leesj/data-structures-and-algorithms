d66b92d3a10023919e453f7e6e3292115f596290
Crystal Soo Lee <soo.leesj@gmail.com>
2020-02-02 23:18:04 -0800

    finished fizzbuzztree

--- code401challenges/README.md
+++ code401challenges/README.md
@@ -412,3 +412,25 @@ O(n)
 [Link to code](https://github.com/crystal-leesj/data-structures-and-algorithms/tree/master/code401challenges/src/main/java/code401challenges/tree/BinaryTree.java)
 
 ***
+
+# Challenge Summary
+Conduct “FizzBuzz” on a tree while traversing through it. Change the values of each of the nodes dependent on the current node’s value
+
+## Challenge Description
+- Write a function called `FizzBuzzTree` which takes a tree as an argument.
+- Without utilizing any of the built-in methods available to your language, determine whether or not the value of each node is divisible by 3, 5 or both. Create a new tree with the same structure as the original, but the values modified as follows:
+    - If the value is divisible by 3, replace the value with “Fizz”
+    - If the value is divisible by 5, replace the value with “Buzz”
+    - If the value is divisible by 3 and 5, replace the value with “FizzBuzz”
+    - If the value is not divisible by 3 or 5, simply turn the number into a String.
+- Return the new tree.
+
+## Approach & Efficiency
+O(n)
+
+## Solution
+![alt text](https://github.com/crystal-leesj/data-structures-and-algorithms/blob/master/code401challenges/assets/fizzBuzzTree.jpg "FizzBuzzTree")
+
+[Link to code](https://github.com/crystal-leesj/data-structures-and-algorithms/tree/master/code401challenges/src/main/java/code401challenges/utilities/FizzBuzzTree.java)
+
+***
--- code401challenges/src/test/java/code401challenges/utilities/FizzBuzzTreeTest.java
+++ code401challenges/src/test/java/code401challenges/utilities/FizzBuzzTreeTest.java
@@ -1,10 +1,60 @@
 package code401challenges.utilities;
 
+import code401challenges.tree.BinaryTree;
+import code401challenges.tree.IntNode;
+import code401challenges.tree.TreeNode;
 import org.junit.Test;
 
-public class FizzBuzzTreeTest {
-    @Test
-    public void test() {
+import static org.junit.Assert.assertEquals;
 
+public class FizzBuzzTreeTest {
+    static BinaryTree<Object> tree;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFizzBuzzTree_case1() {
+        tree = new BinaryTree<>();
+        tree.root = new TreeNode<>(1);
+        tree.root.left = new TreeNode<>(2);
+        tree.root.right = new TreeNode<>(3);
+        tree.root.left.left = new TreeNode<>(4);
+        tree.root.left.right = new TreeNode<>(5);
+
+        FizzBuzzTree<Object> testTree = new FizzBuzzTree<Object>();
+        BinaryTree<Object> actual = FizzBuzzTree.fizzBuzzTree(tree);
+
+        System.out.println("actual = " + actual.root.value);
+        assertEquals(1, actual.root.value);
+        assertEquals(2, actual.root.left.value);
+        assertEquals("Fizz", actual.root.right.value);
+        assertEquals(4, actual.root.left.left.value);
+        assertEquals("Buzz", actual.root.left.right.value);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFizzBuzzTree_case2() {
+        tree = new BinaryTree<>();
+        tree.root = new TreeNode<>(15);
+        tree.root.left = new TreeNode<>(8);
+        tree.root.right = new TreeNode<>(3);
+        tree.root.left.left = new TreeNode<>(18);
+        tree.root.left.right = new TreeNode<>(5);
+
+        FizzBuzzTree<Object> testTree = new FizzBuzzTree<Object>();
+        BinaryTree<Object> actual = FizzBuzzTree.fizzBuzzTree(tree);
+
+
+        assertEquals("FizzBuzz", actual.root.value);
+        assertEquals(8, actual.root.left.value);
+        assertEquals("Fizz", actual.root.right.value);
+        assertEquals("Fizz", actual.root.left.left.value);
+        assertEquals("Buzz", actual.root.left.right.value);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFizzBuzzTree_case3() {
+        tree = new BinaryTree<>();
+        FizzBuzzTree<Object> testTree = new FizzBuzzTree<Object>();
+        FizzBuzzTree.fizzBuzzTree(tree);
     }
 }
