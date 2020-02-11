6d7fee3c9a6ad4eb5db681dda4543f729afcc976
Crystal Soo Lee <soo.leesj@gmail.com>
2020-02-11 10:52:22 -0800

    insertion sort code and test code is ready

---
+++ code401challenges/src/main/java/code401challenges/sort/InsertionSort.java
@@ -0,0 +1,19 @@
+package code401challenges.sort;
+
+public class InsertionSort {
+    public static int[] selectionSort(int[] inputArray) {
+        int n = inputArray.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (inputArray[j] < inputArray[min]) {
+                    min = j;
+                }
+            }
+            int temp = inputArray[min];
+            inputArray[min] = inputArray[i];
+            inputArray[i] = temp;
+        }
+        return inputArray;
+    }
+}
